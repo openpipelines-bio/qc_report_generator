3387c5e957deafa92f19a2eaa09a10dc62c6d568
jakubmajercik <jakub.majercik@gmail.com>
2025-05-23 09:19:59 +0200

    fix default filter parsing

--- src/lib/get-data.ts
+++ src/lib/get-data.ts
@@ -14,11 +14,23 @@ function decompress<T>(compressed: string): T {
 }
 
 export async function getData(): Promise<RawData> {
-  const data = await import("~/data/dataset");
+  const data = await import("~/data/dataset_cb_imm");
   return decompress<RawData>(data.compressed_data)
 }
 
 export async function getReportStructure(): Promise<ReportStructure> {
-  const data = await import("~/data/report_structure")
-  return decompress<ReportStructure>(data.compressed_data);
+  const data = await import("~/data/report_structure_cellranger");
+  const reportStructure = decompress<ReportStructure>(data.compressed_data);
+  
+  // Convert nulls to undefined for cutoffs and zoom levels
+  reportStructure.categories.forEach(category => {
+    category.defaultFilters.forEach(filter => {
+      if (filter.cutoffMin === null) filter.cutoffMin = undefined;
+      if (filter.cutoffMax === null) filter.cutoffMax = undefined;
+      if (filter.zoomMin === null) filter.zoomMin = undefined;
+      if (filter.zoomMax === null) filter.zoomMax = undefined;
+    });
+  });
+  
+  return reportStructure;
 }
